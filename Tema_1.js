172c0004f95567c6d7cf24871b25e7a1fe64ced4
ScR4PP <stefan.jarcau@student.unitbv.ro>
2023-02-22 10:44:15 +0200

    Nothing new

--- Tema_1.js
+++ Tema_1.js
@@ -3,19 +3,19 @@ console.log(grade, letterGrade(grade));
 
 function letterGrade(grade)
 {
-    grade = Math.round(grade);
+    grade = Math.floor(grade);
     switch (true)
     {
         case (grade >= 60 && grade <= 69):
             return 'D';
 
-        case (grade > 70 && grade <= 79):
+        case (grade >= 70 && grade <= 79):
             return 'C';
         
-        case (grade > 80 && grade <= 89):
+        case (grade >= 80 && grade <= 89):
             return 'B';
 
-        case (grade > 90 && grade <= 100):
+        case (grade >= 90 && grade <= 100):
             return 'A';
         
         default:
