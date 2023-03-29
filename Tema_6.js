e6dd39b568b467201400a3e2a34a0c5658d06737
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2023-03-29 14:46:22 +0300

    Done Tema6

--- Tema_6.js
+++ Tema_6.js
@@ -13,37 +13,44 @@ repeat(1001, (n) => {
   });
 });
 
+const sum_age = (obj) => {
+  return _.reduce(obj, (x, y) => x.age + y.age);
+}
 
-const sum = (x, y) => {
-    return x + y;
+const sum = (arr) => {
+    return _.reduce(arr, (x, y) => x + y);
 }
 
 const mean = (arr) => {
-    return _.reduce(arr, function(x, y) {return x+y}) / arr.length;
+    return (_.reduce(arr, function(x, y) {return x+y}) / arr.length).toFixed(2);
 }
 
-const median = (x, y) => {
-    return (x + y) / 2;
+const median = (arr) => {
+    sorted = arr.sort((a, b) => a - b);
+    middle = Math.floor(sorted.length / 2);
+    return sorted.length % 2 === 0 ? (sorted[middle - 1] + sorted[middle]) / 2 : sorted[middle];
 }
 
-const min = (x, y) => {
-    return x < y ? x : y;
+const min = (arr) => {
+    return _.reduce(arr, (x, y) => x < y ? x : y);
 }
 
-const max = (x, y) => {
-    return x > y ? x : y;
+const max = (arr) => {
+    return _.reduce(arr, (x, y) => x > y ? x : y);
 }
 
 function aggregator(arr, agg) {
-    if (agg.name == "mean")
-    {
-        return mean(arr);
-    }
-    return _.reduce(arr, agg);
+    return agg(arr);
 }
 
-console.log(aggregator([1, 2, 3, 4, 5], sum));
-console.log(aggregator([1, 2, 3, 4, 5], mean));
-console.log(aggregator([1, 2, 3, 4, 5], median));
-console.log(aggregator([1, 2, 3, 4, 5], min));
-console.log(aggregator([1, 2, 3, 4, 5], max));
+var people = [
+  { name: "Fred", age: 65, health: 3 },
+  { name: "Lucy", age: 36, health: 5 },
+];
+
+console.log("\nSuma: " + aggregator([8,4,6,7,1,3], sum));
+console.log("Suma varsta: " + aggregator(people, sum_age));
+console.log("Media: " + aggregator([8,4,6,7,1,3], mean));
+console.log("Medianul: " + aggregator([8,4,6,7,1,3], median));
+console.log("Min: " + aggregator([8,4,6,7,1,3], min));
+console.log("Max: " + aggregator([8,4,6,7,1,3], max));
